c17812103fe855d1993dbe9ed6dcda5191853305
Rojan Mainali <rojanmainali@Rojans-MacBook-Air.local>
2026-07-29 09:23:17 +0545

    helper test

---
+++ frontend/test/helpers/testData.js
@@ -0,0 +1,10 @@
+export const TEST_USER = {
+  email: 'demo@aqualife.com',
+  password: 'password123',
+  name: 'Demo User',
+};
+
+export const TEST_ADMIN = {
+  email: 'admin@aqualife.com',
+  password: 'password123',
+};
